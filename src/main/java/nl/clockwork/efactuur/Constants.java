f114eb123ed6284724d74ab5f25b18ce30710cff
eluinstra <edwin.luinstra@clockwork.nl>
2013-07-18 15:36:46 +0200

    moved ubl and setu constants from module digifact to project efactuur-pdf-nl

---
+++ src/main/java/nl/clockwork/efactuur/Constants.java
@@ -0,0 +1,20 @@
+package nl.clockwork.efactuur;
+
+public class Constants
+{
+	public static final String MESSAGE_VERSION_UBL_old = "old";
+	public static final String MESSAGE_VERSION_UBL_1_1 = "1.1";
+	public static final String MESSAGE_VERSION_UBL_1_6_2 = "1.6.2";
+	public static final String MESSAGE_VERSION_UBL_1_6_3 = "1.6.3";
+	public static final String MESSAGE_VERSION_UBL_1_7 = "1.7";
+	public static final String MESSAGE_VERSION_UBL_1_8_beta2 = "1.8.beta2";
+	public static final String MESSAGE_VERSION_UBL_DEFAULT = MESSAGE_VERSION_UBL_1_8_beta2;
+			
+	public static final String MESSAGE_VERSION_SETU_1_1 = "1.1";
+	public static final String MESSAGE_VERSION_SETU_1_6_4 = "1.6.4";
+	public static final String MESSAGE_VERSION_SETU_1_7 = "1.7";
+	public static final String MESSAGE_VERSION_SETU_1_8 = "1.8";
+	public static final String MESSAGE_VERSION_SETU_1_8_1_beta01 = "1.8.1.beta01";
+	public static final String MESSAGE_VERSION_SETU_DEFAULT = MESSAGE_VERSION_SETU_1_8_1_beta01;
+
+}
